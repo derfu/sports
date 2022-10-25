513f2bb40003142762b4e7f4e6e20e552f046a81
derfu <derrickfu@gmail.com>
2022-10-24 22:56:09 -0700

    updating scoring logic

--- sports.py
+++ sports.py
@@ -30,7 +30,9 @@ for i in data['items']:
     if i['scoreValue'] == 6:
         PAT = i["pointAfterAttempt"]
 
-    if type['id'] == 26: ## pick
+    if type['id'] == 7 or type['id'] == 9: ## 7 is regular sack, 9 is sack fumble with own recovery
+        score2 += 0.5
+    elif type['id'] == 26: ## pick
         score2 += 0.5
     elif type['id'] == 36: ## pick 6
         score2 += 1.5
@@ -76,7 +78,7 @@ for i in data['items']:
         out.write(line)
 
     
-    if i['team']['$ref'] == "http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022/teams/8?lang=en&region=us":
+    if i['team']['$ref'] == "http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022/teams/17?lang=en&region=us":
         team1_score += score1
         team2_score += score2
     else:
